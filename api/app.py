88bc5e44763e2f1fc2d6d9c37eb17b67d012059e
mekjr1 <gafar_meque@hotmail.com>
2023-06-16 16:24:14 -0600

    finished adding db and analyzing with Sonar

--- api/app.py
+++ api/app.py
@@ -3,14 +3,18 @@ from sklearn.ensemble import RandomForestClassifier
 import json
 import numpy as np
 import os
-
 from passenger import Passenger
 from apix import RandomForestAPI
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 
 
 api = RandomForestAPI('random_forest_model.pkl')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///predictions.db'
+db = SQLAlchemy(app)
 
 class PassengerEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -23,11 +27,23 @@ class PassengerEncoder(json.JSONEncoder):
         elif isinstance(obj, np.int64):
             return int(obj)  # Convert int64 to int
         return super().default(obj)
+    
+class Prediction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    passenger_id = db.Column(db.Integer)
+    prediction = db.Column(db.Integer)
 
 
 @app.route('/', methods=['GET'])
 def test():
     return jsonify({'message': 'It works! Now you have to call the right endpoint'})
+
+@app.route('/predictions', methods=['GET'])
+def get_predictions():
+    predictions = Prediction.query.all()
+    response = [{'passenger_id': prediction.passenger_id, 'prediction': prediction.prediction} for prediction in predictions]
+    return jsonify(response)
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -46,7 +62,14 @@ def predict():
                 int(data['Age*Class'])  # Convert to int
             )
             prediction = api.predict_single(passenger)
-            response = {'prediction': int(prediction)}  # Convert prediction to int
+            prediction = int(prediction)
+            response = {'prediction': prediction}
+
+            # Save the prediction to the database
+            passenger_id = 1  # Replace with the actual passenger ID
+            prediction_entry = Prediction(passenger_id=passenger_id, prediction=prediction)
+            db.session.add(prediction_entry)
+            db.session.commit()
         else:
             response = {'error': 'Invalid data format. Required keys: Pclass, Sex, Age, Fare, Embarked, Title, IsAlone, Age*Class'}
     elif isinstance(data, list):  # Batch prediction
@@ -65,7 +88,18 @@ def predict():
                 for passenger_data in data
             ]
             predictions = api.predict_batch(passengers)
-            response = {'predictions': [int(prediction) for prediction in predictions]}  # Convert predictions to int
+            predictions = [int(prediction) for prediction in predictions]
+            response = {'predictions': predictions}
+
+            # Save the predictions to the database
+            passenger_ids = [1, 2, 3]  # Replace with the actual passenger IDs
+            prediction_entries = [
+                Prediction(passenger_id=passenger_id, prediction=prediction)
+                for passenger_id, prediction in zip(passenger_ids, predictions)
+            ]
+            db.session.add_all(prediction_entries)
+            db.session.commit()
+    
         else:
             response = {'error': 'Invalid data format. Required keys: Pclass, Sex, Age, Fare, Embarked, Title, IsAlone, Age*Class'}
     else:
@@ -76,4 +110,6 @@ def predict():
 port = int(os.environ.get('PORT', 5000))
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all() 
     app.run(debug=True, host='0.0.0.0', port=port)
