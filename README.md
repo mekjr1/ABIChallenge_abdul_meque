86c83010d5062602c61679b2776c652e12204658
mekjr1 <gafar_meque@hotmail.com>
2023-06-21 15:49:30 -0600

    June 20 Commit

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ docker image build -t api_docker .
 
 docker run -p 5000:5000 -d api_docker
 
+
+
+[![SonarCloud](https://sonarcloud.io/images/project_badges/sonarcloud-white.svg)](https://sonarcloud.io/summary/new_code?id=mekjr1_ABIChallenge_abdul_meque)
--- api/passenger.py
+++ api/passenger.py
@@ -1,15 +1,15 @@
 # passenger.py
 class Passenger:
     def __init__(self, pclass, sex, age, fare, embarked, title, is_alone, age_class):
-        self.Pclass = pclass
-        self.Sex = sex
-        self.Age = age
-        self.Fare = fare
-        self.Embarked = embarked
-        self.Title = title
-        self.IsAlone = is_alone
-        self.AgeClass = age_class
+        self.pclass = pclass
+        self.sex = sex
+        self.age = age
+        self.fare = fare
+        self.embarked = embarked
+        self.title = title
+        self.isAlone = is_alone
+        self.ageClass = age_class
     
     def get_features(self):
         # Return the features as a list or array for prediction
-        return [self.Pclass, self.Sex, self.Age, self.Fare, self.Embarked, self.Title, self.IsAlone, self.AgeClass]
+        return [self.pclass, self.sex, self.age, self.fare, self.embarked, self.title, self.isAlone, self.ageClass]
